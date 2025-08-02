b38166e5ffe4f579dba2e94e7c60b5255e68a963
Rasel raaz <raselraaz07@gmail.com>
2025-08-02 22:33:35 +0600

    Homa sdk added

---
+++ Assets/Scripts/HomaSdkInit.cs
@@ -0,0 +1,28 @@
+using HomaGames.HomaBelly;
+using UnityEngine;
+
+public class HomaSdkInit : MonoBehaviour
+{
+    public void Awake()
+    {
+        if (!HomaBelly.Instance.IsInitialized)
+        {
+            // Listen event for initialization
+            Events.onInitialized += OnHomaBellyInitialized;
+        }
+        else
+        {
+            OnHomaBellyInitialized();
+        }
+    }
+
+    private void OnDisable()
+    {
+        Events.onInitialized -= OnHomaBellyInitialized;
+    }
+
+    private void OnHomaBellyInitialized()
+    {
+        // Homa Belly initialized, call any Homa Belly method
+    }
+}
